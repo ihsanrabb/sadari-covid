55880ec93345edbc4a0e08fdb5d903732c92f4a1
ihsanrabb <ihsanrabbani25@gmail.com>
2020-09-20 12:33:35 +0700

    update chart data from new api

--- components/CovidChart.js
+++ components/CovidChart.js
@@ -3,17 +3,25 @@ import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import styles from '../styles/components/CovidChart.module.scss'
 import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';  
 
 
 const CovidChart = ({casesData}) => {
+
   const [confirmed, setConfirmed] = useState('')
   const [recovered, setRecovered] = useState('')
   const [deaths, setDeaths] = useState('')
+  const [totalConfirmed, setTotalConfirmed] = useState('')
+  const [totalRecovered, setTotalRecovered] = useState('')
+  const [totalDeaths, setTotalDeaths] = useState('')
 
   useEffect(() => {
-    filterNumber(setConfirmed, casesData.confirmed.value)
-    filterNumber(setRecovered, casesData.recovered.value)
-    filterNumber(setDeaths, casesData.deaths.value)
+    filterNumber(setConfirmed, casesData.update.penambahan.jumlah_positif)
+    filterNumber(setRecovered, casesData.update.penambahan.jumlah_sembuh)
+    filterNumber(setDeaths, casesData.update.penambahan.jumlah_meninggal)
+    filterNumber(setTotalConfirmed, casesData.update.total.jumlah_positif)
+    filterNumber(setTotalRecovered, casesData.update.total.jumlah_sembuh)
+    filterNumber(setTotalDeaths, casesData.update.total.jumlah_meninggal)
   });
 
   function filterNumber(state, data) {
@@ -23,27 +31,27 @@ const CovidChart = ({casesData}) => {
   return (
     <Container>
       <h3>Statistik COVID-19 di Indonesia</h3>
-      <p>15 September 2020 berdasarkan www.covid19.go.id</p>
+      <p>{casesData.update.penambahan.tanggal} berdasarkan www.covid19.go.id</p>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <Paper elevation={3} className={styles.positif_wrap}>
             <h5>Positif</h5>
-            <p>+3.507</p>
-            <span>{confirmed}</span>
+            <p>+{confirmed}</p>
+            <span>{totalConfirmed}</span>
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper elevation={3} className={styles.sembuh_wrap}>
             <h5>Sembuh</h5>
-            <p>+3.507</p>
-            <span>{recovered}</span>
+            <p>+{recovered}</p>
+            <span>{totalRecovered}</span>
           </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper elevation={3} className={styles.meninggal_wrap}>
             <h5>Meninggal</h5>
-            <p>+3.507</p>
-            <span>{deaths}</span>
+            <p>+{deaths}</p>
+            <span>{totalDeaths}</span>
           </Paper>
         </Grid>
       </Grid>
@@ -51,6 +59,8 @@ const CovidChart = ({casesData}) => {
   )
 }
 
-
+CovidChart.propTypes = {
+  casesData: propTypes.object
+}
 
 export default CovidChart
