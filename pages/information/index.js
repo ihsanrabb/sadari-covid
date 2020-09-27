a44d1c5f7dfa3e142a7b00b996e3be912158cbdc
ihsanrabb <ihsanrabbani25@gmail.com>
2020-09-27 23:42:17 +0700

    load data from news collection

--- pages/information/index.js
+++ pages/information/index.js
@@ -1,10 +1,32 @@
 import BottomNavigation from '../../components/BottomNavigation'
 import InformationList from '../../components/InformationList'
+import { useEffect, useState } from 'react'
+import { db } from '../../firebase'
 
 const Information = () => {
+  const [newsList, setNewsList] = useState([])
+
+  useEffect(() => {
+    db.collection("news").orderBy("date", "desc").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        let data = {
+          title: doc.data().title,
+          date: doc.data().date.toDate(),
+          sub_description: doc.data().sub_description,
+          description: doc.data().description,
+          imageURL: doc.data().imageURL
+        }
+        setNewsList(prevList => [...prevList, {
+          id:doc.id,
+          data:data
+        }])
+      });
+    });
+  }, [])
+
   return (
     <>
-      <InformationList />
+      <InformationList newsList={newsList} />
       <BottomNavigation className="navigation-bottom" />
     </>
   )
