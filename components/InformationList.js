2649f2fc56aa2a9782725d06d9bb5d1a46cf5346
ihsanrabb <ihsanrabbani25@gmail.com>
2020-09-27 23:42:38 +0700

    loop data from newsList

--- components/InformationList.js
+++ components/InformationList.js
@@ -1,146 +1,36 @@
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import styles from '../styles/components/InformationList.module.scss'
+import Link from 'next/link'
 
-const InformationList = () => {
+const InformationList = (props) => {
   return (
     <Container>
       <h3>Informasi Terbaru</h3>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Contemplative Reptile"
-                height="120"
-                image="/img/dummy-covid.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Covid 19 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-
-      </Grid>
+      <div className={styles['container-list']}>
+      {props.newsList.map((news) => (
+        <div className={styles['news-wrap']} key={news.id}>
+          <Link href={`/information/[id]`} as={`/information/${news.id}`}>
+            <Grid container>
+              <Grid item xs={4}>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="120"
+                  image={news.data.imageURL}
+                  title="Contemplative Reptile"
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <h2>{news.data.title}</h2>
+                <p>{news.data.date.toLocaleString('id-ID')}</p>
+              </Grid>
+            </Grid>
+          </Link>
+        </div>
+      ))}
+      </div>
     </Container>
   )
 }
